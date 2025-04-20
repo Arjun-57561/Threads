4343ec19f7d6378fd881fe15154aff9e5d75e9d2
Arjun-57561 <mallikarjun57561@gmail.com>
2025-04-20 18:04:24 +0530

    feat: Add ThreadThree.java

---
+++ ThreadThree.java
@@ -0,0 +1,15 @@
+public class ThreadThree extends Thread {
+    public void run() {
+        printSquares();
+    }
+    public void printSquares() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread Three - Square: " + (i * i));
+            try {
+                Thread.sleep(600); // pause for 600 ms
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted.");
+            }
+        }
+    }
+}
